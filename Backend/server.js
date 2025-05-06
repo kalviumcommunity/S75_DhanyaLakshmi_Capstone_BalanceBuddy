9ad6e3a45455a7d663af5ce96da5fd83eeaa6222
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-05-06 18:03:03 +0530

    Database_read_and_write_performed

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app=express();
 require('dotenv').config();
 app.use(express.json());
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes')
 
+
+const URI = process.env.URI;
 const PORT = process.env.PORT;
+app.use('/',userRoutes)
 
-app.listen((PORT),()=>{
-    console.log(`server running on http://localhost:${PORT}`)
+mongoose.connect(URI)
+.then(()=>{
+    console.log("Database Connected Successfully!")
+    app.listen((PORT),()=>{
+        console.log(`server running on http://localhost:${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Failed to connect",err)
 })
