6d17c53c3bdfaaddafd9f68b536c95ac0b8f6314
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-11-18 16:51:15 +0530

    Fixing vite-config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,24 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-redirects',
-      closeBundle() {
-        const src = path.resolve(__dirname, 'public/_redirects');
-        const dest = path.resolve(__dirname, 'dist/_redirects');
-
-        if (fs.existsSync(src)) {
-          fs.copyFileSync(src, dest);
-          console.log('✔ _redirects file copied to dist/');
-        } else {
-          console.warn('⚠ _redirects file not found in public/');
-        }
-      }
-    }
-  ]
+  plugins: [react()],
 });
